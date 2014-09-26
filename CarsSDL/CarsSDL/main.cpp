517d19d45694af1431f97d682869e08de5b86729
Rybo5000 <mentalrybo@hotmail.com>
2014-09-26 14:51:23 +1000

    Worst Fears 7B
    Sorcery
    
    Destroy target Ryan's arrogance.
    
    "You thought too much of yourself and that was your downfall" - Ryan, to
    himself.

--- CarsSDL/CarsSDL/main.cpp
+++ CarsSDL/CarsSDL/main.cpp
@@ -10,5 +10,22 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
+	SDL_Window* window = SDL_CreateWindow("Font Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 600, 400, SDL_WINDOW_SHOWN);
+
+	SDL_Surface* screen = SDL_GetWindowSurface(window);
+	TTF_Font* font = TTF_OpenFont("lazy.ttf", 28);
+	SDL_Color textColor = { 110, 255, 0 };
+
+	SDL_Surface* message = TTF_RenderText_Solid(font, "The quick brown fox jumps over the lazy dog", textColor);
+
+	SDL_Rect offset;
+	offset.x = 40;
+	offset.y = 40;
+
+	SDL_BlitSurface(message, NULL, screen, &offset);
+
+	int test;
+	std::cin >> test;
+	
 	return 0;
 }
